ecc41546a3c8687a6aa56e41dc740924783a3f10
KL <34787092+DuppyIsCool@users.noreply.github.com>
2023-11-09 00:35:15 -0500

    Reset Items Code

--- Assets/ResetItems.cs
+++ Assets/ResetItems.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class ResetItems : MonoBehaviour
 {
+    public Vector3 spawnLocation;
+    public float resetDistance = 15.0f; 
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Initialize the spawn location to the current position at the time of awakening
+        spawnLocation = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Check the distance from the current position to the spawn location
+        if (Vector3.Distance(transform.position, spawnLocation) > resetDistance)
+        {
+            //If it's greater than resetDistance, reset the position
+            transform.position = spawnLocation;
+        }
     }
 }
